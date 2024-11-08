9ebe12b07da52a2b8f55a333a8c248ccc2efa9f7
Karla Torres <torrestrujillokarla@gmail.com>
2024-11-08 07:05:59 -0600

    PRACTICA EMPLEADOS TORRES KARLA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
-        path: 'auth',
-        loadChildren: () => import('./auth/features/auth.routes')
-    }
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/features/auth.routes'),
+  },
+  {
+    path: 'formularios',
+    loadChildren: () => import('./formularios/formularios.routes'), // Asegúrate de que sea el módulo correcto
+  },
 ];
+
